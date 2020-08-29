62ff866ab204099b315228e2ffd4722982799dfd
Frederico Braga <frederico.braga@x-team.com>
2020-08-29 19:26:11 -0300

    add popup add buttons

---
+++ popup.js
@@ -0,0 +1,15 @@
+let btnRateBad = document.getElementById('btnRateBad')
+let btnRateMeh = document.getElementById('btnRateMeh')
+let btnRateGood = document.getElementById('btnRateGood')
+
+btnRateBad.onclick = function (element) {
+  alert('Bad!')
+}
+
+btnRateMeh.onclick = function (element) {
+  alert('Meh!')
+}
+
+btnRateGood.onclick = function (element) {
+  alert('Good!')
+}

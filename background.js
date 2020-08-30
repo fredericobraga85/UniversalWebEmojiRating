f2ea86cf3d84c12feb184e3006bd6ddac836884a
Frederico Braga <frederico.braga@x-team.com>
2020-08-29 22:29:51 -0300

    add local rate

--- background.js
+++ background.js
@@ -1,44 +1,51 @@
-// chrome.runtime.onInstalled.addListener(function () {
-//   rateUrl(0)
-// })
 chrome.tabs.onActivated.addListener(onActiveTabChange)
 
 function onActiveTabChange() {
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
     let url = tabs[0].url
-    saveCurrentUrl(url)
+    saveCurrentUrl(url, function () {
+      //fetch rate of current url and updateIcon
+      updateIcon()
+    })
     // use `url` here inside the callback because it's asynchronous!
   })
 }
 
-function saveCurrentUrl(data) {
-  chrome.storage.local.set({ currentUrl: data.url }, function () {
-    //fetch rate of current url and updateIcon
-    updateIcon()
+function saveCurrentUrl(url, callback) {
+  chrome.storage.local.set({ currentUrl: url }, function () {
+    callback()
   })
 }
 
 function rateUrl(rate) {
-  chrome.storage.local.set({ rate }, function () {
-    updateIcon()
+  chrome.storage.local.get('currentUrl', function (data) {
+    chrome.storage.local.set({ [data.currentUrl]: rate }, function () {
+      //fetch rate of current url and updateIcon
+      updateIcon()
+    })
   })
 }
 
 function updateIcon() {
-  chrome.storage.local.get(['rate', 'currentUrl'], function (data) {
-    var rate = data.rate
+  chrome.storage.local.get('currentUrl', function (data) {
+    var currentUrl = data.currentUrl
+    chrome.storage.local.get([data.currentUrl], function (data) {
+      var rate = data[currentUrl]
+      chrome.browserAction.setBadgeText({ text: String(rate) })
 
-    chrome.browserAction.setBadgeText({ text: String(rate) })
-
-    if (rate === 0) {
-      chrome.browserAction.setIcon({ path: 'images/bad-face-32.png' })
-      chrome.browserAction.setBadgeBackgroundColor({ color: '#900' })
-    } else if (rate === 2.5) {
-      chrome.browserAction.setIcon({ path: 'images/neutral-face-32.png' })
-      chrome.browserAction.setBadgeBackgroundColor({ color: '#888' })
-    } else {
-      chrome.browserAction.setIcon({ path: 'images/good-face-32.png' })
-      chrome.browserAction.setBadgeBackgroundColor({ color: '#090' })
-    }
+      if (rate === 0) {
+        chrome.browserAction.setIcon({ path: 'images/bad-face-32.png' })
+        chrome.browserAction.setBadgeBackgroundColor({ color: '#900' })
+      } else if (rate === 2.5) {
+        chrome.browserAction.setIcon({ path: 'images/neutral-face-32.png' })
+        chrome.browserAction.setBadgeBackgroundColor({ color: '#888' })
+      } else if (rate === 5) {
+        chrome.browserAction.setIcon({ path: 'images/good-face-32.png' })
+        chrome.browserAction.setBadgeBackgroundColor({ color: '#090' })
+      } else {
+        chrome.browserAction.setBadgeText({ text: '' })
+        chrome.browserAction.setIcon({ path: 'images/good-face-32.png' })
+      }
+    })
   })
 }
